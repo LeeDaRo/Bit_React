c57013606742cd69f14d4c3d0a8853322ac850b4
LeeDaRo <pokl001@naver.com>
2022-07-21 17:04:46 +0900

    init

---
+++ CH00_준비하기/Javascript/ex01_consol.js
@@ -0,0 +1,21 @@
+// mdn을 보고 공부하세영 ㅋ_ㅋ
+console.log("주현아~~~~~ 츄르먹자");
+// import '.App.css';
+// import Calcuator from '/Calculator';
+// //import TemperatureInput from 'TemperatureInput';
+// //한글입력!!
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Calcuator />
+//       </header>
+//     </div>
+//   )
+// }
+// export default App;
+console.log("주현아 츄르~~ 먹어야지~~?");
+let 주현아 = "츄르먹자~";
+console.log("주현아", 주현아);
+console.log(typeof (주현아));
+console.log("값은 %s 입니다", 주현아);
